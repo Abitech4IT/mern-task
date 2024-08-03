b1d24e219196373af15b396da1ad57b7558d306e
Abideen Abdulazeez <abitechit@gmail.com>
2024-08-03 09:16:42 +0100

    feat:frontend-wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,68 @@
+import { Container, Grid, Typography } from "@mui/material";
+import TextInput from "./components/TextInput";
+import Button from "./components/Button";
+import SelectInput from "./components/SelectInput";
+
 function App() {
-  return <div>Hello Task</div>;
+  return (
+    <Container
+      sx={{
+        backgroundColor: "lightblue",
+        height: "400px",
+        marginTop: "100px",
+        padding: 5,
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Typography variant="h3" align="center">
+            Task Management System
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <TextInput
+            title="Task Title"
+            name="task"
+            placeholder="Enter your task"
+            size="small"
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button type="submit" variant="contained" sx={{ marginTop: 4 }}>
+            Add Task
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <SelectInput
+            size="small"
+            name="country"
+            value=""
+            title={
+              <Typography fontSize={16} fontWeight={600}>
+                Filter By
+              </Typography>
+            }
+            menuItems={[
+              { title: "Completed", value: "completed" },
+              { title: "Pending", value: "pending" },
+            ]}
+            sx={{
+              marginBottom: 3,
+              p: 0.5,
+              borderRadius: 1,
+              outline: "none",
+              border: "1px solid",
+              boxShadow: "none",
+              ".MuiOutlinedInput-notchedOutline": {
+                borderStyle: "none",
+              },
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/TextInput.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  FormControl,
+  FormHelperText,
+  TextField,
+  SxProps,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import InputAdornment from "@mui/material/InputAdornment";
+
+export interface TextInputPros {
+  title?: string | React.ReactNode;
+  name: string;
+  value?: any;
+  placeholder?: string;
+  size?: "small" | "medium";
+  select?: boolean;
+  disabled?: boolean;
+  multiline?: boolean;
+  rows?: number;
+  childlabel?: string[];
+  onChange?: (event: any) => void;
+  sx?: SxProps;
+  error?: string;
+  icon?: React.ReactNode;
+  iconPosition?: "start" | "end";
+}
+
+const TextInput = (props: TextInputPros) => {
+  return (
+    <FormControl variant="outlined" fullWidth>
+      <Typography fontSize={14} fontWeight={500} mb={0.7}>
+        {props.title}
+      </Typography>
+      <TextField
+        InputProps={{
+          "aria-label": "weight",
+          startAdornment: props.icon && (
+            <InputAdornment position={props?.iconPosition || "start"}>
+              {props.icon}
+            </InputAdornment>
+          ),
+        }}
+        value={props.value}
+        placeholder={props?.placeholder}
+        name={props.name}
+        size={props.size}
+        disabled={props.disabled}
+        multiline={props.multiline}
+        rows={props.rows}
+        select={props.select}
+        onChange={props?.onChange}
+        sx={{
+          p: 0.5,
+          borderRadius: 1,
+          outline: "none",
+          border: "1px solid",
+          boxShadow: "none",
+          ".MuiOutlinedInput-notchedOutline": {
+            borderStyle: "none",
+          },
+          ...props.sx,
+        }}
+      />
+      <FormHelperText sx={{ color: "red" }}>{props?.error}</FormHelperText>
+    </FormControl>
+  );
+};
+
+export default TextInput;
